702e9fdffe1dc528d23340a84b077f697a364e18
sfaradam <sfar.adam@esprit.tn>
2025-06-08 14:49:43 +0100

    final

--- src/skips.jsx
+++ src/skips.jsx
@@ -40,19 +40,19 @@ function Skips() {
                     <svg className="h-5 w-5 text-teal-400 mt-0.5 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                       <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7"></path>
                     </svg>
-                    <span>Next-day delivery available</span>
+                    <span>Next-day delivery available.</span>
                   </li>
                   <li className="flex items-start">
                     <svg className="h-5 w-5 text-teal-400 mt-0.5 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                       <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7"></path>
                     </svg>
-                    <span>No hidden fees</span>
+                    <span>No hidden fees.</span>
                   </li>
                   <li className="flex items-start">
                     <svg className="h-5 w-5 text-teal-400 mt-0.5 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                       <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7"></path>
                     </svg>
-                    <span>24/7 customer support</span>
+                    <span>24/7 customer support.</span>
                   </li>
                 </ul>
               </div>

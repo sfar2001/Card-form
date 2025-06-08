d8f4de2e3e3029b394ef41e4dac0d2c2736de377
sfaradam <sfar.adam@esprit.tn>
2025-06-08 15:06:19 +0100

    final with db.json

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,6 @@
 import axios from 'axios';
  
-const API_URL = 'http://localhost:3001/skips';
- 
+const API_URL = '/db.json'; 
 export const fetchskip = async () => {
   try {
     const response = await axios.get(API_URL);
@@ -10,23 +9,4 @@ export const fetchskip = async () => {
     console.error("Error fetching skips:", error);
     return []; // Return an empty array if the fetch fails
   }
-};
-export const fetchskipById = async (id) => {
-  const response = await axios.get(`${API_URL}/${id}`);
-  return response.data;
-};
- 
-export const createskip = async (prop) => {
-  const response = await axios.post(API_URL, prop);
-  return response.data;
-};
- 
-export const updateskip = async (id, prop) => {
-  const response = await axios.put(`${API_URL}/${id}`, prop);
-  return response.data;
-};
- 
-export const deleteskip = async (id) => {
-  const response = await axios.delete(`${API_URL}/${id}`);
-  return response.data;
 };

b179cfa5c870fc2309c0a9edd071de97bc27e867
sfaradam <sfar.adam@esprit.tn>
2025-06-08 14:34:31 +0100

    final

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
   return (
      <Router>
      <Routes>
-        <Route path="/skips" element={<Skips/>} />
+        <Route path="/" element={<Skips/>} />
       </Routes>
     </Router>
   )
